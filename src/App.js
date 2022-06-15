b04d7ab073c41f68d5d15c32ef77a2c0bf566fe3
James Allard <jd.allard@aol.com>
2022-06-14 19:40:15 -0500

    hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Header from "./components/Header";
-import CampsitesList from "./features/campsites/CampsitesList";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import "./App.css";
 import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
