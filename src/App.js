00a6eb4a9eff58a1343e9fe388d04f2e05a8c4f6
James Allard <jd.allard@aol.com>
2022-06-16 19:14:19 -0500

    homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/Homepage";
 import Header from "./components/Header";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import "./App.css";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
